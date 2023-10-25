25c3ffb6f9c04fd92cfcd1bc885551621d697884
Selen Dilek <yildirimselen17@gmail.com>
2023-10-25 13:44:11 -0400

    hamcrest library.

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,6 +1,8 @@
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
 
@@ -42,14 +44,41 @@ public class ZippoTest {
                 .statusCode(200); //test kismi oldugundan adderstion status code 200 mu 201 yazinca hata veriyor
 
 
+    }
+
+    @Test
+    public void contentTypeTest(){
+
+        given()
+                //hazirlik kismi bos
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
 
 
 
-
-
-
+                .then()
+                .log().body() // donen body json data, log().All() : data disindaki her sey
+                .statusCode(200)
+                .contentType(ContentType.JSON); // donen datanin tipi JSON mi
 
 
     }
 
+    @Test
+    public void test3(){
+        //bir bilginin degerini
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                      .then()
+                      .log().body() //donen degerlerin tumunu json data deniliyor. posymande body=pm.response.json(); ile aliyorduk -> body.country
+                .statusCode(200) //assertion
+                .body("country",equalTo("United States")) //assertion ->kutuphane ekleyecez kendi icinde test edecez
+
+        ;
+
+    }
 }
