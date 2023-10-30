a92f7ff3d32a668ffe198d9c616b4d2dae04ab81
Selen Dilek <yildirimselen17@gmail.com>
2023-10-30 13:39:16 -0400

    fasterxml.jackson

---
+++ src/test/java/Model/Location.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Location {
+     private String postcode;
+     private String country;
+     private String countryabbreviation;
+     private ArrayList<Place> places;
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryabbreviation() {
+        return countryabbreviation;
+    }
+
+    public void setCountryabbreviation(String countryabbreviation) {
+        this.countryabbreviation = countryabbreviation;
+    }
+
+    public ArrayList<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(ArrayList<Place> places) {
+        this.places = places;
+    }
+}
--- src/test/java/_03_ApiTestPOJO.java
+++ src/test/java/_03_ApiTestPOJO.java
@@ -1,2 +1,33 @@
+import Model.Location;
+import org.testng.annotations.Test;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.*;
+
 public class _03_ApiTestPOJO {
+//POJO : JSON nesnesi : locationNesnesi (JSON dilini javaya ceviriyoruz)
+    //kullanimi kolaylastirmak icin yapiyoruz. veriler var kalibini hazirliyoruz donusum yapip kullanacza
+
+    @Test
+    public void extractJsonAll_POJO(){
+        //Ogrnci cinsinden ogr gibi Ogrenci ogr1 = new Ogrenci();
+        Location location =
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .extract().body().as(Location.class) //Location Kalibina gore(Model paketinden)Location bilgilerini body den al yaz
+                //donen body bilgisini al Location Class kalibi ile cevir (fakat donusumu saglayan bir dependencies eklemek lazim JSONi eski haline dondurmek lazim)
+                //developer serializer yapip JSON a donusturmus biz de deserializer yapip java ya cevirecez
+
+
+                ;
+
+
+
+    }
+
+
 }
