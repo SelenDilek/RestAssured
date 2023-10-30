2e14faee8b15b238336676c8e10a6ce12648767c
Selen Dilek <yildirimselen17@gmail.com>
2023-10-30 16:30:14 -0400

    Task has done.

--- src/test/java/_04_Tasks.java
+++ src/test/java/_04_Tasks.java
@@ -1,9 +1,31 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 public class _04_Tasks {
     /**
-     create a request to https://httpstat.us/203
-     expect status 203
-     expect content type TEXT
+     create a request to https://jsonplaceholder.typicode.com/todos/2
+     expect status 200
+     expect content type JSON
+     expect title in response body to be "quis ut nam facilis et officia qui"
      */
+
+@Test
+    public void task3() {
+
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title", equalTo("quis ut nam facilis et officia qui"))
+        ;
+
+    }
 
 
 
